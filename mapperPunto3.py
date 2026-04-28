bae5cb23ef1186a2e3a153b4d873017bac2b751e
Manvelmont1 <83312521+Manvelmont1@users.noreply.github.com>
2026-04-28 16:48:50 -0500

    Update mapperPunto3.py

--- mapperPunto3.py
+++ mapperPunto3.py
@@ -1,11 +1,9 @@
-## PUNTO 3: Cuantos alimentos tienen mas de 0.1 gramos de tiamina
-## Mapper - Lee todo el Dataset y cuenta los alimentos que cumplen la condicion
+## N. de alimentos que tienen mas de 0.1 gramos de tiamina
 
-## Lectura del archivo
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 lineas = open("./Dataset.csv", encoding="utf-8").read().splitlines()
-lineas = lineas[1:]  ## Eliminar encabezado
+lineas = lineas[1:]
 
 consolidado = {
     "cantidad": 0
@@ -14,7 +12,6 @@ consolidado = {
 for linea in lineas:
     tiamina_str = linea.split(";")[7].strip().replace(",", ".")
 
-    ## Ignorar valores no numericos (guiones)
     if tiamina_str == "-":
         tiamina_str = "0"
 
@@ -24,13 +21,11 @@ for linea in lineas:
         consolidado["cantidad"] = consolidado["cantidad"] + 1
 
 print(consolidado)
-
-## Guardar resultado en un archivo para que el reducer pueda leerlo
 resultado = ""
 
 for clave in consolidado:
     resultado = resultado + str(clave) + ";" + str(consolidado[clave]) + "\n"
 
 archivo = open("resultadoPunto3Mapper.csv", "w", encoding="utf-8")
-archivo.write(resultado[:-1])  ## Elimina el ultimo caracter (en este caso es un salto de linea)
+archivo.write(resultado[:-1]) 
 archivo.close()
